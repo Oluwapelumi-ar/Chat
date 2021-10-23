24e68d600230ad2f11353b8d98580fa64c15c9a9
Shukurah Bakare <shukurahbakare@outlook.com>
2021-10-23 13:40:40 +0100

    MESSAGE

--- src/app/model/chatMessageDot.ts
+++
@@ -1,9 +0,0 @@
-export class ChatMessageDot {
-    user:String = '';
-    message: String = '';
-
-    constructor(user: string,message:string) {
-        this.user = user;
-        this.message = message;
-    }
-}
