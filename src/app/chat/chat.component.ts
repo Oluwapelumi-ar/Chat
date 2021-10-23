6dedf4231576f1b6f3fc48f759669993f7745072
Shukurah Bakare <shukurahbakare@outlook.com>
2021-10-23 13:55:44 +0100

    reinitializing git

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ChatMessageDot } from '../model/chatMessageDot';
+import { WebSocketService } from '../services/web-socket.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+    displaymsgs:ChatMessageDot[] = [];
+    message: any = {user:'', msg:''};
+  constructor(public webSockerService: WebSocketService) { }
+  
+
+  ngOnInit(): void {
+    this.webSockerService;
+    this.receive();
+  }
+
+  receive(){
+    this.webSockerService.listen().subscribe((data:any) => {
+     
+      this.displaymsgs.push(data) 
+     console.log(this.displaymsgs);
+     return;
+      
+      
+    })
+  }
+
+  sendMessage(sendForm: NgForm){
+    let sent = this.webSockerService.sendMessage(sendForm.value)
+    sendForm.controls.message.reset();
+    return  sent ;
+    
+  }
+
+
+}
